1902e52741f70ce07f368fe7557e2b7c3a7df8d2
phil <philipthecow@yahoo.com>
2018-01-13 21:25:05 -0500

    unit test now working between dir

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,15 @@
 #include <gtest/gtest.h>
 
+#include "someFile.hpp"
+
 TEST(somename, somefunction){
 	ASSERT_EQ(1,1);
 }
 
 int main(int argc, char* argv[])
 {
+	sayHi();
+	
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
